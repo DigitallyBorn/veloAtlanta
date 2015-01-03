5920234eea3252b57393bdc946de1acdbdcb1e19
Casmith109 <casmith109@gmail.com>
2015-01-02 19:31:52 -0500

    added some new designs to the webapp

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,11 @@ router.get('/', function(req, res) {
   res.render('index', 
   	{ title: 'Velo Atlanta Cycling Club',
   	titleShort: 'Velo Atlanta', 
-  	article: 'This is the article that i have been trying to get to work for a long time now and i hope that this will finally work.' 
+  	article: 'This is the article that i have been trying to get to work for a long time now and i hope that this will finally work.', 
+  	membersTitle: 'Loved by our members!',
+  	membersSubTitle: 'This is the best riding group that I have ever been a part of.  I would recommend this group to anyone looking to get in shape.',
+  	joinTitle: 'Why join velo!',
+  	joinSubTitle: 'Read up, on what Velo can do to help you be the best person that you can possibly be'
   });
 });
 
