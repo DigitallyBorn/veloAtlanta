e37a5006defc877907041085d1ee83d0ab5d2fa1
Ricky Smith <ricky@digitally-born.com>
2015-01-11 10:48:38 -0500

    Removed url value from member api resource

--- src/html/app/app.api.members.js
+++ src/html/app/app.api.members.js
@@ -1,7 +1,7 @@
 (function(angular) {
-  var membersApi = function($resource, api_base_url) {
-    return $resource(api_base_url + 'members/:id');
+  var membersApi = function($resource) {
+    return $resource('/members/:id');
   };
 
-  angular.module('app').factory('membersApi', ['$resource', 'api_base_url', membersApi]);
+  angular.module('app').factory('membersApi', ['$resource', membersApi]);
 })(angular);
--- src/html/app/app.values.js
+++ src/html/app/app.values.js
@@ -1,5 +1,4 @@
 (function(angular) {
   var app = angular.module('app');
 
-  app.constant('api_base_url', 'http://localhost:3001/api/');
 })(angular);
